f7989b0d230b4c933d6ead0e2f3db709ab4e81bf
Vishal Prasad Singh <vishalprasadsingh52@gmail.com>
2025-03-06 09:36:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.java
+++ Difficulty: Basic/Array Search/array-search.java
@@ -33,13 +33,14 @@ class Solution {
 
     static int search(int arr[], int x) {
 
-       for(int i=0; i<=arr.length-1; i++)
-       {
-           if(arr[i]==x)
-           {
-               return i;
-           }
-       }
-       return -1;
+        // Your code here
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==x)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
